e1e450c529f417aa52ca10c423386784db90510a
Igor <gianeri.vieira@gmail.com>
2025-09-09 22:16:35 -0300

    aula lista

---
+++ aula005/lista.py
@@ -0,0 +1,11 @@
+#Criando uma lista vazia
+lista_vazia = []
+
+#Criando uma lista com elementos
+numeros = [1, 2, 3, 4, 5]
+
+#Criando uma lista com elementos de diferentes tipos de dados
+lista_mista = [1, "dois", 3.0]
+
+#Criando uma lista com outra lista dentro dela
+lista_aninhada = [[1, 2], [3,4], [5,6]]
